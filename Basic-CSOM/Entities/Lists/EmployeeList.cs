66b2364b2efff66e6fcbf4ace81c9c2de9d02883
Thanh Pham Duy <thanh.pham.duy@preciofishbone.se>
2021-05-04 16:19:11 +0700

    Add 3 list

---
+++ Basic-CSOM/Entities/Lists/EmployeeList.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.SharePoint.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic_CSOM.Entities.Lists
+{
+    public class EmployeeList : BaseList
+    {
+        public EmployeeList(ClientContext context) : base(context)
+        {
+            Title = "EmployeeList";
+            ShowColumns = new List<string>()
+            {
+                "ShortDesc",
+                ""
+            };
+            ContentTypeName = "EmployeeTestList";
+
+
+        }
+    }
+}
